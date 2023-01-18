88703b98fc3740139177eb8d80419996592b6e11
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-19 02:02:42 +0800

    update

--- devices/armvirt_pcat/diy.sh
+++ devices/armvirt_pcat/diy.sh
@@ -5,10 +5,8 @@ bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
 mv -f target/linux/armvirt target/linux/armvirt_pcat
 
-svn co https://github.com/photonicat/rockchip_rk3568_openwrt_packages/trunk/utils/pcat-manager feeds/packages/utils/pcat-manager
-ln -sf $SHELL_FOLDER/../../feeds/packages/utils/pcat-manager package/feeds/packages/pcat-manager
-
-svn co https://github.com/openwrt/packages/branches/openwrt-21.02/devel/meson feeds/packages/devel/meson
+#svn co https://github.com/photonicat/rockchip_rk3568_openwrt_packages/trunk/utils/pcat-manager feeds/packages/utils/pcat-manager
+#ln -sf $SHELL_FOLDER/../../feeds/packages/utils/pcat-manager package/feeds/packages/pcat-manager
 
 
 svn co https://github.com/photonicat/rockchip_rk3568_openwrt/trunk/files files
